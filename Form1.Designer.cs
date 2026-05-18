e84ac093e1174451d0122bd240f305bafb57bdc0
nicollas <nicollas.b@aluno.ifsp.edu.br>
2026-05-17 23:32:23 -0300

    Delete Form1.Designer.cs

--- Form1.Designer.cs
+++
@@ -1,5 +0,0 @@
-            // timerFade
-            // 
-            this.timerFade.Enabled = false;
-            this.timerFade.Interval = 30;
-            this.timerFade.Tick += new System.EventHandler(this.timerFade_Tick);
