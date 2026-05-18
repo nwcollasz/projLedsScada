6a915879da812228a65b159d4cea10021775ac96
nicollas <nicollas.b@aluno.ifsp.edu.br>
2026-05-17 23:33:04 -0300

    Delete Form1.cs

--- Form1.cs
+++
@@ -1,7 +0,0 @@
-        private void timerFade_Tick(object sender, EventArgs e)
-        {
-            if (this.Opacity < 1)
-                this.Opacity += 0.40; 
-            else
-                timerFade.Stop();
-        }
